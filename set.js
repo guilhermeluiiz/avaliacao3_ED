f4a8dac8a2a9dfdd346d018eadd018124b59dcde
vitorleonel120hz <vitor.leonel@acad.ifma.edu.br>
2026-01-07 17:02:35 -0300

    Adicionando a classe set

---
+++ set.js
@@ -0,0 +1,88 @@
+class Set {
+  constructor() {
+    this.items = [];
+    this.count = 0;
+  }
+
+  //Adiciona elementos na lista sendo que o valor e o próprio valor e a chave e o valor não pode ser adicionado duas vezes no mesmo conjunto.
+  add(element) {
+    if (!this.has(element)) {
+      this.items[element] = element;
+      console.log(`O elemento ${element} foi adicionado na lista.`);
+      this.count++;
+    } else {
+      console.log(`O elemento ${element} já está na lista.`);
+    }
+  }
+
+  //Remove um elemento pelo elemento e pelo valor.
+  delete(element) {
+    if (this.has(element)) {
+      delete this.items[element];
+      console.log(`O elemento ${element} foi excluido com suceso.`);
+      this.count--;
+    } else {
+      console.log(`O elemento não está presente na lista.`);
+    }
+  }
+
+  //Retorna verdadeiro se o elemento está presente dentro de items
+  has(element) {
+    if (element in this.items) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  clear() {
+    this.items = [];
+    this.count = 0;
+  }
+  size() {
+    console.log(`Existe ${this.count} elmentos no conjunto.`);
+  }
+  values() {
+    return Object.values(this.items);
+  }
+
+  union(otherSet) {
+    const unionSet = new Set();
+    this.values().forEach((value) => unionSet.add(value));
+    otherSet.values().forEach((value) => unionSet.add(value));
+    console.log(`O conjunto união dos 2 conjuntos são: ${unionSet.values()}`);
+    return unionSet;
+  }
+
+  intersection(otherSet) {
+    const intersectionSet = new Set();
+    const values = this.values();
+    for (let i = 0; i < values.length; i++) {
+      if (otherSet.has(values[i])) {
+        intersectionSet.add(values[i]);
+      }
+    }
+    return intersectionSet;
+  }
+
+  difference(otherSet) {
+    const differenceSet = new Set();
+    this.values().forEach((value) => {
+      if (!otherSet.has(value)) {
+        differenceSet.add(value);
+      }
+    });
+    return differenceSet;
+  }
+}
+
+const setA = new Set();
+const setB = new Set();
+setA.add(2);
+setA.add(1);
+setA.add(3);
+
+setB.add(7);
+setB.add(1);
+setB.add(3);
+setB.add(8);
